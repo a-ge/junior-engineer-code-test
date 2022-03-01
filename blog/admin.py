1a9953c7f93cdc5e92b2af3f4b21785196e107f0
a-ge <amgesmundo@gmail.com>
2022-03-01 12:18:19 -0800

    also registered HitCount in admin.py to connect model to app.

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, HitCount
 
 
 class PostAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Post, PostAdmin)
-
+admin.site.register(HitCount)
