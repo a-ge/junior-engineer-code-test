e90269f63d7b05e2b15633945d45a9dab08fef7e
a-ge <amgesmundo@gmail.com>
2022-02-28 22:52:08 -0800

    updated posted_at = models.DateTimeField(auto_now=True) to fix bug.

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(max_length=2048)
     posted_by = models.CharField(max_length=50, default="root")
-    posted_at = models.DateTimeField(default=auto_now)
+    posted_at = models.DateTimeField(auto_now=True)
     edited_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
