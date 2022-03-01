01671f126aceec1d5e7073567fc98286504c2abb
a-ge <amgesmundo@gmail.com>
2022-03-01 12:14:43 -0800

    edited Post posted_at field to auto_now_add and added HitCount model.

--- blog/models.py
+++ blog/models.py
@@ -8,9 +8,15 @@ class Post(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(max_length=2048)
     posted_by = models.CharField(max_length=50, default="root")
-    posted_at = models.DateTimeField(auto_now=True)
+    posted_at = models.DateTimeField(auto_now_add=True)
     edited_at = models.DateTimeField(auto_now=True)
-    hit_count = models.PositiveBigIntegerField(default=0)
 
     def __str__(self):
         return self.title
+
+class HitCount(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.PROTECT)
+    hit_count = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        return str(self.hit_count)
